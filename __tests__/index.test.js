c2ab09b43af720ccd90615121dd147168a43e03c
vasily.smolin <vasilysmoolin@gmail.com>
2022-08-26 15:39:54 +0400

    test js and yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,26 @@
-it('init', () => {
-  expect(true).toBeTruthy();
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+import genDiff from '../index';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const testList = [
+  'yaml',
+  'json',
+];
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
+
+const result = readFixture('expectedStylish');
+
+describe('gendiff', () => {
+  test.each(testList)('gendiff %s', (format) => {
+    const filepath1 = getFixturePath(`file1.${format}`);
+    const filepath2 = getFixturePath(`file2.${format}`);
+    expect(genDiff(filepath1, filepath2)).toEqual(result);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(result);
+  });
 });
