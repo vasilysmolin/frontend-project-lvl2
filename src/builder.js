841988466bfd65699d737d9e2094a58958221042
vasily.smolin <vasilysmoolin@gmail.com>
2022-08-27 11:30:58 +0400

    nested yaml and json file

--- src/builder.js
+++ src/builder.js
@@ -9,13 +9,13 @@ const buildDiff = (data1, data2) => {
     if (!_.has(data2, key)) {
       return { key, type: 'delete', value: data1[key] };
     }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { key, type: 'nested', children: buildDiff(data1[key], data2[key]) };
+    }
     if (!_.isEqual(data1[key], data2[key])) {
       return {
         key, type: 'change', value1: data1[key], value2: data2[key],
       };
-    }
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { key, type: 'nested', children: buildDiff(data1[key], data2[key]) };
     }
 
     return { key, type: 'unchanged', value: data2[key] };
